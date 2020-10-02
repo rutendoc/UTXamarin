f57a5ff7fa7cedbb049b3af7a94df1e4a6a5c723
rutendoc <rchibvupe@gmail.com>
2020-10-02 08:17:03 +0200

    Add project files.

---
+++ UTXamarin/UTXamarin/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTXamarin.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ UTXamarin/UTXamarin/Models/PasswordRecoveryModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTXamarin.Models
+{
+    public class PasswordRecoveryModel
+    {
+        public string Email { get; set; }
+    }
+}
